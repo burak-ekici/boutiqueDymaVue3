f98c0212b97014b2ba0887ac3af3fba05ffac440
Ekici-Burak <b.ekici@it-students.fr>
2022-12-09 16:25:38 +0100

    initialisation du projet / variable css

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Bonjour {{ name }}</h1>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const name = ref("jean");
+</script>
+
+<style lang="scss">
+@import "./assets/base.scss";
+@import "./assets/debug.scss";
+</style>
